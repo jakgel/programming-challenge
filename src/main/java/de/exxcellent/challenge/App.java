1d17eaa5838a8e21c19e2a2b13fb3adde94b6e06
jakobg <jakob.gelszinnis@web.de>
2018-05-14 13:04:06 +0200

    Followed https://commons.apache.org/proper/commons-csv/user-guide.html (Example: Parsing an Excel CSV File) to provide the first implementation of reading .csv columns via their header identifiers.
    I read https://www.callicoder.com/java-read-write-csv-file-apache-commons-csv/ to get an handle on how to use this Appache Open CSV.
    I realized that it might be not very straightforward to access all elements as an array. After reading https://www.tutorialspoint.com/java/java_arraylist_class.htm I also thought that using an ArrayList object might become handy.
    But first, I add to admit that I was quite unsure about the common way to store table output in java. Hence I decided to start simple and change the first concern
    'Read CSV' into 'Load the three relevant columns' within loadfile().
    
    This will become handy later in compare_columns().

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,8 +1,11 @@
 package de.exxcellent.challenge;
 
 import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+
+import java.io.*;
+import java.util.*;
+
 
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
@@ -13,15 +16,19 @@ import org.apache.commons.csv.CSVRecord;
  */
 public final class App {
 
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
 
         // Your preparation code …
 
 
         // invoke the constructor
-        TableDataset ds = new TableDataset("Testname");
+        TableDataset ds1 = new TableDataset("weather.csv");
         // invoke the method
-        ds.loadfile();
+        ds1.loadfile("Day", "MxT","MnT");
+
+        TableDataset ds2 = new TableDataset("football.csv");
+        ds2.loadfile("Team", "Goals","Goals Allowed");
+
 
         String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
         String teamWithSmallesGoalSpread = "A good team"; // Your goal analysis function call …
@@ -32,22 +39,53 @@ public final class App {
 }
 
 
+
 class TableDataset {
-
-
+    /*
+    The custom class that will lead us through the challenge
+     */
+    private String relative = "src/main/resources/de/exxcellent/challenge/";
     private String filename;
+    private ArrayList<String> colid;
+    private ArrayList<Float> colA; //float
+    private ArrayList<Float> colB; //float
 
+//    private ArrayList<String> countmin;
+//    private float argmin;
 
-    /*  THe constructor   */
+    /*  The constructor   */
     TableDataset(String filename) {
         this.filename = filename;
     }
 
-    void loadfile() {
-        System.out.printf("This is a test : %s%n", this.filename);
+    void loadfile(String colid, String colA, String colB) throws IOException {
+        /*
+        This function should sets the file and save the three columns of interest
+       */
+
+        // The id will be strings, the  column values will be floats, because we take the difference between them.
+        this.colid = new ArrayList<>();
+        this.colA  = new ArrayList<>();
+        this.colB  = new ArrayList<>();
+
+        Reader input = new FileReader(String.format("%s%s", this.relative, this.filename));
+        Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(input);
+        for (CSVRecord record : records) {
+            this.colid.add(record.get(colid));
+            this.colA.add(Float.valueOf(record.get(colA)));
+            this.colB.add(Float.valueOf(record.get(colB)));
+
+        }
+
+        System.out.printf("Colid: %s%n", this.colid);
+        System.out.printf("ColA : %s%n", this.colA );
+        System.out.printf("ColB : %s%n", this.colB);
     }
 
 }
+
+
+
 
 
 
